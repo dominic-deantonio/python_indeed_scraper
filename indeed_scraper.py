b0ddb9874d2060ea37c53477294ce620bc2ddf3d
Dominic <dominicdeantonio@icloud.com>
2022-01-13 16:56:39 -0500

    Improvements

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -1,45 +1,73 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def _safe_extract_text(obj):
+    # Safely extracts text by checking HTML element for None
+
     return getattr(obj, 'text', '').strip()
 
+
 def _safe_extract_location(obj):
+    # Safely extracts locations by checking HTML element for None
+
     out = []
     for loc in obj.contents:
         out.append(_safe_extract_text(loc))
 
     return out
 
+
+def _extract_from_job_element(job_element):
+    # Extract the elements from the main job_element
+    title_element = job_element.find("h2", class_="jobTitle")
+    company_element = job_element.find("span", class_="companyName")
+    company_location_element = job_element.find("div", class_="companyLocation")
+    salary_range_element = job_element.find("div", class_="metadata salary-snippet-container")
+    job_snippet_element = job_element.find("div", class_="job-snippet")
+
+    # Extract the text from the children elements and return from function
+    return {        
+        "title": _safe_extract_text(title_element.contents[-1]), # Sometimes has multiple children, so we extract the last one
+        "company": _safe_extract_text(company_element),
+        "location": _safe_extract_location(company_location_element),
+        "salary_range": _safe_extract_text(salary_range_element),
+        "job_snippet": _safe_extract_text(job_snippet_element),
+        "link": "https://indeed.com" + job_element['href'],
+        "source_id": job_element['id']
+    }
+
+
 def scrape(url):
+    # Main scraping function
+
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
+
+    # This holds the job elements
     results = soup.find(id="mosaic-provider-jobcards")
-    job_elements = results.find_all("a")        
+
+    if results is None:
+        print('Unable to get results :(')
+        return []
+
+    # Gets the HTML Elements containing jobs
+    job_elements = results.find_all("a")
 
     jobs = []
 
+    # Loop over all the extracted job elements to extract the text
     for job_element in job_elements:
-        title_element = job_element.find("h2", class_="jobTitle")
 
+        # Do a test to make sure the the title is available
+        title_element = job_element.find("h2", class_="jobTitle")
         if title_element is None:
             continue
 
-        # Extract the elements from the main job_element
-        company_element = job_element.find("span", class_="companyName")
-        company_location_element = job_element.find( "div", class_="companyLocation")
-        salary_range_element = job_element.find("div", class_="metadata salary-snippet-container")
-        job_snippet_element = job_element.find("div", class_="job-snippet")
+        jobs.append(_extract_from_job_element(job_element))
 
-        # Extract the text from the children elements and append it to the list
-        jobs.append({        
-            "title": _safe_extract_text(title_element.contents[-1]), # Sometimes has multiple children
-            "company": _safe_extract_text(company_element),        
-            "location": _safe_extract_location(company_location_element), # Sometimes has multiple children
-            "salary_range": _safe_extract_text(salary_range_element),
-            "job_snippet": _safe_extract_text(job_snippet_element),
-            "link": "https://indeed.com" + job_element['href'],
-            "source_id" : job_element['id']
-        })
+    if(len(jobs) == 0):
+        print(
+            f'Unable to get jobs. There may have been a problem with the raw HTML:\n\n{page.content}')
 
     return jobs
--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 import json
+from art import show_art
+from indeed_scraper import scrape
+import time
 
-from indeed_scraper import scrape as indeed
+"""
+Main script for the user to interface with the scraping function
+"""
 
-url = "https://www.indeed.com/jobs?q=software%20developer&vjk=26d8d5d099e6295b"
-jobs = indeed.scrape(url)
+show_art()
+
+print('What category of job would you like to scrape?')
+category = input()
+
+print('Enter location or type "n" for none')
+location = input()
+
+if location.lower().strip() == 'n':
+    print('No location selected')
+    location = ""
+    
+# Build the scraping URL from the user input
+url = f"https://www.indeed.com/jobs?q={category}&l={location}" 
+
+# Do the scraping and advise the user
+print(f'Scraping {category} jobs...')
+jobs = scrape(url)
+print(f'Results returned: {len(jobs)}')
+
+if len(jobs) == 0:
+    print('There were no results. Please try again with a different query.')
+    exit()
+
+# Prompt the user to check if they want to save the results
+# Also manage incorrect input
+should_save = ''
+while should_save != 'y' and should_save != 'n':
+    print('Would you like to save the results? "y" or "n"')
+    should_save = input().lower().strip()
+    if should_save != 'y' and should_save != 'n':
+        print('Invalid input')
+
 json_jobs = json.dumps(jobs, indent=4)
 
-print(json_jobs)
+# Do the save if the user wants to save as a file
+if should_save == 'y':
+    file_name =  f'indeed-search-{round(time.time() * 1000)}.json'
+    f = open(file_name, "w")
+    f.write(json_jobs)
+    f.close()
+    print(f'Results saved as {file_name}')
+    time.sleep(2)
+
+print(f'Results:\n\n{json_jobs}')
